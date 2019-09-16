560afd65a865fb754515ecdd364f87b2fbaa5b26
unknown <zeusocean1707@gmail.com>
2019-09-17 00:16:16 +0700

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                Laravel
+                Laravel Test
             </div>
 
             <div class="links">
